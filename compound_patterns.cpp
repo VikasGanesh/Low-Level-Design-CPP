e2ac74c59602bac347ac082a7c125ddeabfb1495
Vikas G <39690844+VikasGanesh@users.noreply.github.com>
2025-03-29 11:31:40 +0530

    Update compound_patterns.cpp

--- compound_patterns.cpp
+++ compound_patterns.cpp
@@ -1,100 +1,204 @@
-class Quackable {
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class QuackObservable;
+
+// Observer Interface
+class Observer {
+public:
+    virtual void update(QuackObservable* duck) = 0;
+};
+
+// Iterator Template
+template <typename T>
+class Iterator {
+public:
+    virtual T getNext() = 0;
+    virtual bool hasNext() = 0;
+};
+
+// Observer Iterator
+class ObserverIterator : public Iterator<Observer*> {
+private:
+    int index;
+    vector<Observer*>& observers;
+public:
+    ObserverIterator(vector<Observer*>& observers) : observers(observers), index(0) {}
+    Observer* getNext() {
+        return hasNext() ? observers[index++] : nullptr;
+    }
+    bool hasNext() {
+        return index < observers.size();
+    }
+};
+
+// Observable Interface
+class QuackObservable {
+public:
+    virtual void registerObserver(Observer* observer) = 0;
+    virtual void notifyObservers() = 0;
+};
+
+// Observable Implementation
+class Observable : public QuackObservable {
+private:
+    vector<Observer*> observers;
+    QuackObservable* duck;
+    unordered_set<Observer*> observerSet;
+public:
+    Observable(QuackObservable* duck) : duck(duck) {}
+    
+    void registerObserver(Observer* observer) {
+        if (observerSet.find(observer) == observerSet.end()) { // Avoid duplicate registration
+            observers.push_back(observer);
+            observerSet.insert(observer);
+        }
+    }
+    
+    void notifyObservers() {
+        ObserverIterator iterator(observers);
+        while (iterator.hasNext()) {
+            iterator.getNext()->update(duck);
+        }
+    }
+};
+
+// Quackable Interface
+class Quackable : public QuackObservable {
 public:
     virtual void quack() = 0;
 };
 
-class MallardDuck : public Quackable  {
+// Concrete Ducks
+class MallardDuck : public Quackable {
+private:
+    Observable observable;
 public:
+    MallardDuck() : observable(this) {}
     void quack() {
-        cout<<"Quack"<<endl;
+        cout << "Quack" << endl;
+        notifyObservers();
+    }
+    void registerObserver(Observer* observer) {
+        observable.registerObserver(observer);
+    }
+    void notifyObservers() {
+        observable.notifyObservers();
     }
 };
 
-class RedheadDuck : public Quackable  {
+class RedheadDuck : public Quackable {
+private:
+    Observable observable;
 public:
+    RedheadDuck() : observable(this) {}
     void quack() {
-        cout<<"Quack"<<endl;
+        cout << "Quack" << endl;
+        notifyObservers();
+    }
+    void registerObserver(Observer* observer) {
+        observable.registerObserver(observer);
+    }
+    void notifyObservers() {
+        observable.notifyObservers();
     }
 };
 
-class DuckCall : public Quackable  {
+class DuckCall : public Quackable {
+private:
+    Observable observable;
 public:
+    DuckCall() : observable(this) {}
     void quack() {
-        cout<<"Kwak"<<endl;
+        cout << "Kwak" << endl;
+        notifyObservers();
+    }
+    void registerObserver(Observer* observer) {
+        observable.registerObserver(observer);
+    }
+    void notifyObservers() {
+        observable.notifyObservers();
     }
 };
 
-class RubberDuck : public Quackable  {
+class RubberDuck : public Quackable {
+private:
+    Observable observable;
 public:
+    RubberDuck() : observable(this) {}
     void quack() {
-        cout<<"Squeak"<<endl;
+        cout << "Squeak" << endl;
+        notifyObservers();
+    }
+    void registerObserver(Observer* observer) {
+        observable.registerObserver(observer);
+    }
+    void notifyObservers() {
+        observable.notifyObservers();
     }
 };
 
-//Adapter design pattern 
-
-//Adaptee
-class Goose{
+// Goose Adapter
+class Goose {
 public:
     void honk() {
-        cout<<"Honk"<<endl;
+        cout << "Honk" << endl;
     }
 };
-//target is it Quackable;
-//adapter class
+
 class GooseAdapter : public Quackable {
 private:
-    //Adaptee
     Goose* goose;
+    Observable observable;
 public:
-    GooseAdapter(Goose* goose){
-        this->goose = goose;
-    }
+    GooseAdapter(Goose* goose) : goose(goose), observable(this) {}
     void quack() {
         goose->honk();
+        notifyObservers();
+    }
+    void registerObserver(Observer* observer) {
+        observable.registerObserver(observer);
+    }
+    void notifyObservers() {
+        observable.notifyObservers();
     }
 };
 
+// Quackologist Observer
+class Quackologist : public Observer {
+public:
+    void update(QuackObservable* duck) {
+        cout << "Quackologist: Duck just quacked." << endl;
+    }
+};
 
-/*
-We’re going to make those Quackologists happy and give
-them some quack counts. we will use decorator design pattern 
-Let’s create a decorator that gives the ducks some new
-behavior (the behavior of counting) by wrapping them with a
-decorator object. We won’t have to change the Duck code at all.
-*/
-
+// Quack Counter Decorator
 class QuackCounter : public Quackable {
 private:
     Quackable* duck;
     static int numberOfQuacks;
 public:
-    QuackCounter(Quackable* duck){
-        this->duck = duck;
-    }
-
+    QuackCounter(Quackable* duck) : duck(duck) {}
     void quack() {
         numberOfQuacks++;
         duck->quack();
     }
-    
-    static int getgetQuacks(){
+    static int getQuacks() {
         return numberOfQuacks;
+    }
+    void registerObserver(Observer* observer) {
+        duck->registerObserver(observer);
+    }
+    void notifyObservers() {
+        duck->notifyObservers();
     }
 };
 
 int QuackCounter::numberOfQuacks = 0;
 
-/*
-This quack counting is great. We’re learning
-things we never knew about the little quackers.
-But we’re finding that too many quacks aren’t
-being counted. 
-
-
-we will use factory design pattern to create ducks now;
-*/
-
+// Factory Pattern
 class AbstractDuckFactory {
 public:
     virtual Quackable* createMallardDuck() = 0;
@@ -103,61 +207,64 @@ public:
     virtual Quackable* createRubberDuck() = 0;
 };
 
-class DuckFactory : public AbstractDuckFactory {
-public:
-    Quackable* createMallardDuck() {
-        return new MallardDuck();
-    }
-    Quackable* createRedheadDuck() {
-        return new RedheadDuck();
-    }
-    Quackable* createDuckCall() {
-        return new DuckCall();
-    }
-    Quackable* createRubberDuck() {
-        return new RubberDuck();
-    }
-};
-
 class CountingDuckFactory : public AbstractDuckFactory {
 public:
-    Quackable* createMallardDuck() {
-        return new QuackCounter(new MallardDuck());
-    }
-    Quackable* createRedheadDuck() {
-        return new QuackCounter(new RedheadDuck());
-    }
-    Quackable* createDuckCall() {
-        return new QuackCounter(new DuckCall());
-    }
-    Quackable* createRubberDuck() {
-        return new QuackCounter(new RubberDuck());
-    }
+    Quackable* createMallardDuck() { return new QuackCounter(new MallardDuck()); }
+    Quackable* createRedheadDuck() { return new QuackCounter(new RedheadDuck()); }
+    Quackable* createDuckCall() { return new QuackCounter(new DuckCall()); }
+    Quackable* createRubberDuck() { return new QuackCounter(new RubberDuck()); }
 };
 
+// Flock Composite Pattern
+class Flock : public Quackable {
+private:
+    vector<Quackable*> ducks;
+public:
+    void add(Quackable* duck) { ducks.push_back(duck); }
+    void quack() {
+        for (Quackable* duck : ducks) {
+            duck->quack();
+        }
+    }
+    void registerObserver(Observer* observer) {
+        for (Quackable* duck : ducks) {
+            duck->registerObserver(observer);
+        }
+    }
+    void notifyObservers() {}
+};
 
+// Duck Simulator
 class DuckSimulator {
 public:
-    void simulate(AbstractDuckFactory* countingDuckFactory){
-        Quackable* mallardDuck = countingDuckFactory->createMallardDuck();
-        Quackable* readHead = countingDuckFactory->createRedheadDuck();
-        Quackable* duckCall = countingDuckFactory->createDuckCall();
-        Quackable* rubberDuck = countingDuckFactory->createRubberDuck();
-        Quackable* gooseAdapter = new GooseAdapter(new Goose());
-
-        mallardDuck->quack();
-        readHead->quack();
-        rubberDuck->quack();
-        duckCall->quack();
-        gooseAdapter->quack();
+    void simulate(AbstractDuckFactory* factory) {
+        Quackable* mallardDuck = factory->createMallardDuck();
+        Quackable* redheadDuck = factory->createRedheadDuck();
+        Quackable* duckCall = factory->createDuckCall();
+        Quackable* rubberDuck = factory->createRubberDuck();
+        Quackable* goose = new GooseAdapter(new Goose());
         
-        cout<<"The ducks quacked "<<QuackCounter::getgetQuacks()<<endl;
+        Quackologist observer;
+        mallardDuck->registerObserver(&observer);
+        redheadDuck->registerObserver(&observer);
+        duckCall->registerObserver(&observer);
+        rubberDuck->registerObserver(&observer);
+        
+        Flock flock;
+        flock.add(mallardDuck);
+        flock.add(redheadDuck);
+        flock.add(duckCall);
+        flock.add(rubberDuck);
+        
+        flock.quack();
+        cout << "The ducks quacked " << QuackCounter::getQuacks() << " times." << endl;
     }
 };
 
 int main() {
     DuckSimulator simulator;
-    AbstractDuckFactory* countingDuckFactory = new CountingDuckFactory();
-    simulator.simulate(countingDuckFactory);
+    AbstractDuckFactory* factory = new CountingDuckFactory();
+    simulator.simulate(factory);
+    delete factory;
     return 0;
 }
